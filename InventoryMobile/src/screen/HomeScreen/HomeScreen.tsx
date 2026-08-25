7a511d713facca65ea0eb895be1c8975188457d8
KhairulAnuar11 <mkabmsh@gmail.com>
2026-08-25 12:11:56 +0800

    basic blank build for screen

---
+++ InventoryMobile/src/screen/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export const HomeScreen: React.FC = () => {
+    return(
+        <View>
+            <Text>HomeScreen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default HomeScreen;
---
+++ InventoryMobile/src/screen/LoginScreen/LoginScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export const LoginScreen: React.FC = () => {
+    return (
+        <View>
+            <Text>LoginScreen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default LoginScreen;
